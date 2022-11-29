c79a03a8542380846641319b0cc53bf1ed4294a7
Bonduellka <117808063+Bonduellka@users.noreply.github.com>
2022-11-29 16:58:21 +0300

    Add files via upload

---
+++ 1_1.pas
@@ -0,0 +1,13 @@
+﻿var mas:array[1..20] of integer;
+    e:integer;
+    kw:real;
+begin
+  for e:= 1 to 20 do 
+12    readln(mas[e]);
+  for e:= 1 to 20 do 
+    if (mas[e] > 0) then
+      mas[e]:= 0
+    else 
+      mas[e]:= mas[e] * mas[e];
+    writeln(mas);
+end.
